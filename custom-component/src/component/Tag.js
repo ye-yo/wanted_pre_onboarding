51093f02021ebbf7d8f9656c89ddc7a7859ea42f
ye-yo <yeyo0x0@gmail.com>
2022-02-07 20:31:51 +0900

    feat(Tag)

---
+++ custom-component/src/component/Tag.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
+
+
+const TagContainer = styled.div`
+    border: 1px solid black;
+    border-radius: 6px;
+    min-width: 340px;
+    min-height: 48px;
+    border: 1px solid #dddddd;
+    padding: 6px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap:wrap;
+    ${({ focusing }) => {
+        if (focusing)
+            return css`
+            border-color: black;
+        `
+    }}
+`
+const TagList = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+`
+const TagBlock = styled.div`
+    display: flex;
+    background-color: ${props => props.theme.colorMain};
+    color: white;
+    padding: 8px;
+    border-radius: 6px;
+    margin-right:6px;
+    
+
+    & > button{
+        background-color: white;
+        border-radius: 50%;
+        margin-left: 4px;
+        width: 18px;
+        height: 18px;
+        padding: 0;
+    }
+`
+const InputTag = styled.input.attrs({ type: 'text' })`
+    flex : 1;
+    padding: 4px;
+    height: 40px;
+    &::placeholder{
+        color: gray;
+    }
+    outline: none;
+    border: none;
+`
+
+let tagCount = 0;
+function Tag() {
+    const [tagList, setTagList] = useState([]);
+    const [text, setText] = useState('');
+    const [focusing, setFocusing] = useState(false);
+    function handleTagRemove(e) {
+        let currentTagList = [...tagList]
+        setTagList(currentTagList.filter((tag, index) => index != e.target.name));
+    }
+
+    function handleTagCreate(e) {
+        const { value } = e.target;
+        if (e.key === 'Enter') {
+            if (value && value.replace(/(\s*)/g, "") != "") {
+                const newTag = { id: tagCount++, tagName: value };
+                setTagList(tagList => [...tagList, newTag])
+            }
+            setText('');
+        }
+    }
+    return (
+        <div>
+            <TagContainer focusing={focusing}>
+                <TagList>
+                    {tagList.map(({ id, tagName }, index) =>
+                        <TagBlock key={id}>
+                            {id}
+                            <p>{tagName}</p>
+                            <button name={index} onClick={handleTagRemove}>x</button>
+                        </TagBlock>
+                    )}
+                </TagList>
+                <InputTag onFocus={() => setFocusing(true)} onBlur={() => setFocusing(false)} onKeyPress={handleTagCreate} placeholder='Press enter to add tags' value={text} onChange={(e) => setText(e.target.value)} ></InputTag>
+            </TagContainer>
+        </div >
+    );
+}
+
+export default Tag;
