821d6fdf2fbe7d7c480e2ca979433fabc76787d9
ye-yo <yeyo0x0@gmail.com>
2022-02-07 19:06:26 +0900

    feat(Modal)

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import theme from './style/theme.js'
 import Layout from "./Layout";
 import Toggle from "./component/Toggle";
+import Modal from "./component/Modal";
 
 const Main = styled.main`
   width:100%;
   height: 100%;
+  display: flex;
+  flex-direction:row;
+  flex-wrap:wrap;
 `
 
 function App() {
@@ -16,6 +20,7 @@ function App() {
         <header>Custom Component</header>
         <Main>
           <Layout><Toggle /></Layout>
+          <Layout><Modal /></Layout>
         </Main>
       </div>
     </ThemeProvider>
---
+++ custom-component/src/component/Modal.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const ButtonOpenModal = styled.button`
+    background-color: ${props => props.theme.colorMain};
+    color: white;
+    padding: 20px;
+    border-radius: 40px;
+`
+const ModalContainer = styled.div`
+    position: fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,.48);
+`
+const ModalBlock = styled.div`
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+    width: 400px;
+    min-height: 120px;
+    max-width: 80%;
+    background-color: white;
+    border-radius: 10px;
+    padding: 10px;
+`
+
+const ButtonCloseModal = styled.button`
+background-color: transparent;
+width: 32px;
+height: 32px;
+margin-bottom: 10px;
+    
+`
+const ModalContent = styled.div`
+`
+function Modal() {
+    const [open, setOpen] = useState(false)
+    return (
+        <tempalte>
+            <ButtonOpenModal onClick={() => setOpen(open => !open)}>Open Modal</ButtonOpenModal>
+            {open &&
+                <ModalContainer>
+                    <ModalBlock>
+                        <ButtonCloseModal onClick={() => setOpen(open => !open)}>X</ButtonCloseModal>
+                        <ModalContent>HELLO CODESTATES!</ModalContent>
+                    </ModalBlock>
+                </ModalContainer>
+            }
+        </tempalte>
+    );
+}
+
+export default Modal;
