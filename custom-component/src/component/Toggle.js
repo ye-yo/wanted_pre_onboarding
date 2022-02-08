438af8a73f45865d669b7a8601b3e32b649bd055
ye-yo <yeyo0x0@gmail.com>
2022-02-08 13:32:18 +0900

    refactor(Toggle): 코드 간결화

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -3,27 +3,18 @@ import styled, { css } from "styled-components";
 
 const defaultWidth = 80, defaultHeight = 34;
 const ToggleContainer = styled.div`
-${({ width, height, theme }) => {
+    display: inline-block;
+    position: relative;
+    ${({ width, height, theme }) => {
         return css`
-            display: inline-block;
             width: ${width}px;
             height: ${height}px;
-            position: relative;
             background-color: ${theme.colorGray};
             border-radius: ${height * 0.5}px;
             overflow: hidden;
-            & > *{
+            &::before{
+                content:"";
                 position: absolute;
-                width: ${height * 0.7}px;
-                height: ${height * 0.7}px;
-                background-color: white;
-                border-radius: 50%;
-                top: 50%;
-                left: ${height * 0.3 / 2}px;
-                transform: translate(0, -50%);
-                transition: left .5s;
-            }  
-            & > ${Paint}{
                 background-color: ${theme.colorMain};
                 width: ${width}px;
                 height:${height}px;
@@ -33,36 +24,41 @@ ${({ width, height, theme }) => {
             } 
         }
         `
-    }
-    }`;
+    }}
 
-const Paint = styled.div``
-const StyledToggle = styled.div``
-const InputCheckBox = styled.input.attrs({ type: 'checkbox' })`
-    ${({ width, height }) => {
+    ${({ width, height, checked }) => {
+        if (checked)
+            return css`
+                &::before{
+                    left:0;
+                }
+                & ${ToggleSwitch}{
+                    left: ${width - (height * (0.3 / 2 + 0.7))}px;
+                }
+            `
+
+    }}
+`;
+const ToggleSwitch = styled.div`
+    ${({ height }) => {
         return css` 
-            :checked{
-                outline: red;
-            }
-            opacity: 0;
+            position: absolute;
+            width: ${height * 0.7}px;
+            height: ${height * 0.7}px;
+            background-color: white;
+            border-radius: 50%;
+            top: 50%;
+            left: ${height * 0.3 / 2}px;
+            transform: translate(0, -50%);
+            transition: left .5s;
             z-index: 1;
             cursor:pointer;
-            &:checked, &:checked +  ${Paint} +${StyledToggle} {
-                left: ${width - (height * (0.3 / 2 + 0.7))}px;
-            }
-            &:checked +  ${Paint} {
-                left : 0;
-            }
         `
     }}
 `
-
-const ToggleStateText = styled.p`
+const ToggleStateText = styled.p` 
     margin: 10px auto 0;
-    ${({ width, height }) => {
-    }}
 `
-
 
 function Toggle({ width, height }) {
     const [checked, setChecked] = useState(false)
@@ -70,10 +66,10 @@ function Toggle({ width, height }) {
         newHeight = height || defaultHeight;
     return (
         <div>
-            <ToggleContainer width={newWidth} height={newHeight}>
-                <InputCheckBox width={newWidth} height={newHeight} checked={checked} onChange={() => setChecked(checked => !checked)}></InputCheckBox>
-                <Paint></Paint>
-                <StyledToggle></StyledToggle>
+            <ToggleContainer width={newWidth} height={newHeight} checked={checked}>
+                <ToggleSwitch width={newWidth} height={newHeight} checked={checked}
+                    onClick={() => setChecked(checked => !checked)}>
+                </ToggleSwitch>
             </ToggleContainer>
             <ToggleStateText checked={checked}>Toggle Switch {checked ? 'ON' : 'OFF'}</ToggleStateText>
         </div>
